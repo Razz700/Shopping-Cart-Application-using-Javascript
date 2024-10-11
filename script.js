bfea3742684bdbec41260ab7bc52c4d8602e36e3
MA Razzakh <160166041+Razz700@users.noreply.github.com>
2024-10-11 11:12:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // myProducts.filter((item)=>item.title.includes(search.value))
 
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+///redirecting if currentuser is present///
+if(localStorage.getItem('currentuser')){
+  let currentuser=JSON.parse(localStorage.getItem('currentuser'));
+  const userdata=JSON.parse(localStorage.getItem('usersdata'));
+  const check=userdata.filter(item=>item.email==currentuser.email && item.password==currentuser.password);
+  if(check.length>0){
+    window.location.href="./profile/profile.html";
+  }
+}
+////redirecting code ends
 let signup=document.getElementById('signup');
 signup.addEventListener('click',()=>{
 window.location.href="signup/signup.html";
